75b8cb6785abe213b7c64da7529fc2f4c24f6f63
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 19:35:13 -0400

    How to use ScrollView in Xamarin Forms

---
+++ ScrollViewXamarinForms/ScrollViewPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ScrollViewXamarinForms
+{
+	public class ScrollViewPage : ContentPage
+	{
+		public ScrollViewPage ()
+		{
+			StackLayout stack = new StackLayout ();
+			for (var i=0; i<100; i++)
+				stack.Children.Add (new Button { Text = "Button "+i});
+			ScrollView scroll = new ScrollView {
+				Content=stack
+			};
+			Content = scroll;
+			Title="ScrollView";
+		}
+	}
+}
+
+
